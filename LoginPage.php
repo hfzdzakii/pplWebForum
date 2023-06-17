171cc581bd74c3d3a3612d93be52f4bfec82bd0f
Farrel Ardra <ardrafarrel@gmail.com>
2023-06-17 10:58:00 +0700

    update login

--- LoginPage.php
+++ LoginPage.php
@@ -21,22 +21,22 @@ $tail = $yielder->getTail();
 <?php echo $head ?>
 <div class="w-[100vw] h-[100vh] bg-[#2B2D42] flex items-center justify-center flex-col	">
     <div class="text-center font-semibold  text-[#D90429] text-4xl pb-10">
-        <p>WEB FORUM</p>
+        <p class="text-6xl">WEB FORUM</p>
     </div>
     
-        <div class="bg-white p-10  rounded shadow-md flex items-center justify-center flex-col">
+        <div class="bg-[#EDF2F4] p-10  rounded shadow-md flex items-center justify-center flex-col">
             <h2 class="text-2xl font-semibold  justify-center">Login</h2>
             <hr class="border border-black w-[600px] my-4">
             <form action="controller.php" method="POST">
                 <div class="mb-4 ">
-                    <input type="username" placeholder="Username" name="UsernameLogin" id="username" class="border border-gray-300 px-3 py-2 rounded w-[354px] h-[45px] " required>
+                    <input type="username" placeholder="Username" name="UsernameLogin" id="username" class="hover:border-2 border-[#2B2D42] px-3 py-2 rounded w-[354px] h-[45px] " required>
                 </div>
                 <div class="mb-6">
-                    <input placeholder="Password" type="password" name="PasswordLogin" id="password" class="border border-gray-300 px-3 py-2 rounded w-[354px] h-[45px]" required>
+                    <input placeholder="Password" type="password" name="PasswordLogin" id="password" class="hover:border-2 border-[#2B2D42] px-3 py-2 rounded w-[354px] h-[45px]" required>
                 </div>
             </form>
             <div class="flex items-center justify-between flex-col mb-4">
-                <button type="submit" name="LoginButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded ">Login</button>
+                <button type="submit" name="LoginButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-[#2B2D42] text-white font-medium px-4 py-2 rounded ">Login</button>
             </div>
             <?php if (isset($_SESSION['error'])) : ?>
                 <div><?php echo $_SESSION['error'];
@@ -46,7 +46,7 @@ $tail = $yielder->getTail();
                 <hr class="border border-black w-[600px] ">
             </div>
             <div class="flex items-center justify-between flex-col ">
-                <button type="submit" name="RegisterButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded">Create New Account</button>   
+                <button type="submit" name="RegisterButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-[#2B2D42] text-white font-medium px-4 py-2 rounded">Create New Account</button>   
             </div>
         </div>
 </div>
