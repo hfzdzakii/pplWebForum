d94bc0faf95e628311e2ae3ae8ffc7d865099132
Azizu Ahmad <azizu.rozaki@gmail.com>
2023-06-17 10:14:07 +0700

    login page rev

---
+++ LoginPage.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include_once('yielder.php');
+$_SESSION['state'] = 'login';
+
+if(isset($_SESSION['login'])){
+    echo "<meta http-equiv='refresh' content='0; url=HomePage.php'>";
+    die();
+}
+if ($_SESSION['state'] == 'login') {
+    $login = true;
+}
+
+
+$yielder = new Yielder($_SESSION['state']);
+$head = $yielder->getHead();
+$tail = $yielder->getTail();
+
+?>
+
+<?php echo $head ?>
+<div class="w-[100vw] h-[100vh] bg-[#2B2D42] flex items-center justify-center flex-col	">
+    <div class="text-center font-semibold  text-[#D90429] text-4xl pb-10">
+        <p>WEB FORUM</p>
+    </div>
+    
+        <div class="bg-white p-10  rounded shadow-md flex items-center justify-center flex-col">
+            <h2 class="text-2xl font-semibold  justify-center">Login</h2>
+            <hr class="border border-black w-[600px] my-4">
+            <form action="controller.php" method="POST">
+                <div class="mb-4 ">
+                    <input type="username" placeholder="Username" name="UsernameLogin" id="username" class="border border-gray-300 px-3 py-2 rounded w-[354px] h-[45px] " required>
+                </div>
+                <div class="mb-6">
+                    <input placeholder="Password" type="password" name="PasswordLogin" id="password" class="border border-gray-300 px-3 py-2 rounded w-[354px] h-[45px]" required>
+                </div>
+            </form>
+            <div class="flex items-center justify-between flex-col mb-4">
+                <button type="submit" name="LoginButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded ">Login</button>
+            </div>
+            <?php if (isset($_SESSION['error'])) : ?>
+                <div><?php echo $_SESSION['error'];
+                        unset($_SESSION['error']); ?></div>
+            <?php endif ?>
+            <div class="flex items-center justify-between flex-col mb-4">
+                <hr class="border border-black w-[600px] ">
+            </div>
+            <div class="flex items-center justify-between flex-col ">
+                <button type="submit" name="RegisterButton" class="bg-[#D90429] w-[354px] h-[45px] hover:bg-blue-600 text-white font-medium px-4 py-2 rounded">Create New Account</button>   
+            </div>
+        </div>
+</div>
+<?php echo $tail?>
--- controller.php
+++ controller.php
@@ -3,9 +3,34 @@ session_start();
 include_once('connectDb.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['something'])) {
-        $_SESSION['pesan'] = '';
+    if (isset($_POST['LoginButton'])) {
+        $username = strtolower(stripslashes($_POST['UsernameLogin']));
+        $password = $_POST['PasswordLogin'];
+        // cek username di db
+        $cek_username = $pdo->prepare("SELECT * FROM 'user' WHERE 'username' = '$username'; ");
+        try{
+            $cek_username->execute();
+            if($cek_username->rowCount()==1){
+                //cek password
+                $baris = $cek_username->fetchAll(PDO::FETCH_ASSOC);
+                if(password_verify($password,$baris[0]['password'])){
+                    $_SESSION['login'] = true;
+                    $_SESSION['username'] = $barisp[0]['username'];
+                    $_SESSION['akses'] = $baris[0]['akses'];
+                    echo "<meta http-equiv='refresh' content='0; url=HomePage.php'>";
+                    die();
+                }
+            }
+        } catch(PDOException $e){
+            echo "Error " . $e->getMessage();
+        }
+        $_SESSION['error'] = 'Usernmae dan Password Tidak Cocok';
+        echo "<meta http-equiv='refresh' content='0; url=LoginPage.php'>";
         die();
+
+        
+        // $_SESSION['pesan'] = '';
+        // die();
     } else {
         unset($_SESSION['login']);
         unset($_SESSION['username']);
