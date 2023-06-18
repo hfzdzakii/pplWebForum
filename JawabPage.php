8a54da9a291791d5e0d202b7d7034310e9bf05da
Farrel Ardra <ardrafarrel@gmail.com>
2023-06-18 12:54:17 +0700

    ck editor

--- JawabPage.php
+++ JawabPage.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include_once('yielder.php');
+include_once('connectDb.php');
 $_SESSION['state'] = 'login';
 
 
@@ -16,19 +17,17 @@ $header = $yielder->getHeader($state);
 <?php echo $head ?>
 <?php echo $header?>
 <?php
-    include_once('connectDb.php');
-    if(isset($_REQUEST['kirimjawaban']))
+    
+    if(isset($_REQUEST['kirimJawaban']))
     {
-        $content = $_REQUEST['content'];
+       $content = $_REQUEST['content'];
 
-        $insert_query = $mysqli_query($connetDb, "insert into jawaban set jawaban='$content'");
-        if($insert_query)
-        {
-            $msg = "DAta masuk";
-        }
-        else 
-        {
-            $msg = "tidak masuk";
+        $insert_query = $pdo->prepare("INSERT into jawaban values(null,1,'$content',0,1,'farel','1')");
+        try {
+            $insert_query->execute();
+            
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
 ?>
@@ -36,8 +35,8 @@ $header = $yielder->getHeader($state);
     <div class="bg-[#EDF2F4] w-4/5">
         <p class="text-[35px]"> JUDUL PERTANYAAN </p>
     </div>
-    <div class="w-4/5">
-        <form action=".php" method="post" enctype="multipart/form-data">
+    <div class="w-4/5 space-y-5">
+        <form action="" method="post" enctype="multipart/form-data">
                 <p class="text-[20px]"> Send as : </p>
                 <input name="ass" type="radio" /> <label> Username </label>
                 <br>
@@ -45,14 +44,18 @@ $header = $yielder->getHeader($state);
                 <textarea name="content" id="content" cols="30" rows="10"></textarea>
                 <button name="kirimJawaban" class="bg-[#D90429] w-[100px] h-[40px] rounded-[5px] text-white border-black border-2"> Send </button>
         </form>
-        <div>
-            <?php if(!empty($msg)) {echo $msg;} ?> 
-        </div>
+        <!-- <div class="text-[20px] text-red-500">
+        </div> -->
 </div>
 <div id="editor"> </div>
                         <script>
                                 ClassicEditor
-                                        .create( document.querySelector( '#content' ) )
+                                        .create( document.querySelector( '#content' ),{
+                                            ckfinder:
+                                            {
+                                                uploadUrl: 'fileupload.php'
+                                            }
+                                        } )
                                         .then( editor => {
                                                 console.log( editor );
                                         } )
